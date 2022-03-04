e9889d41dc4dd09183028dcb1c341ba536d32632
spenserblack <spenserblack01@gmail.com>
2022-03-04 00:37:27 +0000

    Initial commit
    Displays iroiro colors using vercel deployment.

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { colors } from 'iroiro';
+  import TrColor from '../lib/TrColor.svelte';
+</script>
+
+<h1><code>iroiro</code> preview</h1>
+<table>
+  <thead>
+    <th>Hex</th>
+    <th>Romanized</th>
+    <th>Name</th>
+  </thead>
+  <tbody>
+    {#each colors as color}
+      <TrColor {color} />
+    {/each}
+  </tbody>
+</table>
