2caaf662a25938803a949d4bdb7ba9ae50bc3c45
Spenser Black <spenserblack01@gmail.com>
2022-04-26 20:51:37 -0400

    Add dynamic `/:colors` route

---
+++ src/routes/[color].svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import containerBg from '$lib/container-bg';
+  import TrColor from '$lib/TrColor.svelte';
+  import type { Color } from 'iroiro';
+
+  let bgValue = '#FFF';
+  export let colors: Color[] = [];
+
+  containerBg.subscribe((bg) => {
+    bgValue = bg;
+  });
+</script>
+
+<div class="table-container" style="background-color: { bgValue };">
+  <table>
+    <caption>Click on text to copy it to the clipboard</caption>
+    <thead>
+      <th>Hex</th>
+      <th>Romanized</th>
+      <th>Name</th>
+    </thead>
+    <tbody>
+      {#each colors as color}
+        <TrColor {color} />
+      {/each}
+    </tbody>
+  </table>
+</div>
+
+<style>
+  .table-container {
+    padding: 3em;
+  }
+</style>
